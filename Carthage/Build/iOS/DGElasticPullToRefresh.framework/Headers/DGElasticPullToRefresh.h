c2e2cb826b5821d8c2f10ec8513bd5e238770bd5
sgr-ksmt <melodydance.k.s@gmail.com>
2016-03-11 10:03:09 +0900

    Add support Carthage

---
+++ Carthage/Build/iOS/DGElasticPullToRefresh.framework/Headers/DGElasticPullToRefresh.h
@@ -0,0 +1,19 @@
+//
+//  DGElasticPullToRefresh.h
+//  DGElasticPullToRefresh
+//
+//  Created by Suguru Kishimoto on 2016/03/11.
+//  Copyright © 2016年 Danil Gontovnik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DGElasticPullToRefresh.
+FOUNDATION_EXPORT double DGElasticPullToRefreshVersionNumber;
+
+//! Project version string for DGElasticPullToRefresh.
+FOUNDATION_EXPORT const unsigned char DGElasticPullToRefreshVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DGElasticPullToRefresh/PublicHeader.h>
+
+
